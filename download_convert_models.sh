3f3f0c0d48558b35005c8496726c0e8959dc84bc
takeyanova <51348699+takeyanova@users.noreply.github.com>
2019-07-05 22:44:46 +0800

    Update download_convert_models.sh

--- download_convert_models.sh
+++ download_convert_models.sh
@@ -4,5 +4,5 @@
 bash models/download_models.sh
 
 # convert models
-python3 torch_to_pytorch.py --model ../takey/models/vgg_normalised.t7
-python3 torch_to_pytorch.py --model ../takey/models/decoder.t7
+python3 torch_to_pytorch.py --model models/vgg_normalised.t7
+python3 torch_to_pytorch.py --model models/decoder.t7
